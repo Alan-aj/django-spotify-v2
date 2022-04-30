07cb203cb4f998206cefdd8df7be95d1b03189d4
Alan-aj <alanj14333@gmail.com>
2022-05-01 00:27:19 +0530

    static files

--- music_player/settings.py
+++ music_player/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'aj-music.herokuapp.com'
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = ['/static/' , 'django/contrib/admin/static']
+STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
