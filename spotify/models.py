05699974cb87d179c2aa94cec41b52f644eda8ce
Alan-aj <alanj14333@gmail.com>
2022-04-30 22:40:01 +0530

    commit

--- spotify/models.py
+++ spotify/models.py
@@ -5,8 +5,8 @@ from api.models import Room
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    refresh_token = models.CharField(max_length=150, null=True)
-    access_token = models.CharField(max_length=150)
+    refresh_token = models.CharField(max_length=255, null=True)
+    access_token = models.TextField()
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
 
